e03e2467ad803db621506c36e587cc6436716a2b
supapitploy <88095690+supapitploy@users.noreply.github.com>
2022-09-12 16:54:54 +0700

    Update fillStartWord.js

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,12 +2,6 @@ const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
-  if(word === null || word === undefined){
-        return undefined;
-    }
-    if(startWord.includes(word)){     
-        return word;
-    }
-    return startWord + word;
+
 }
 module.exports = fillStartWord
